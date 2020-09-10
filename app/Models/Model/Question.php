83250ef1f09543cb971549d675c2be88694aa385
KenvinNguyenOnline <vanphuconline88#gmail.com>
2020-09-11 00:32:28 +0700

    Relationship

--- app/Models/Model/Question.php
+++ app/Models/Model/Question.php
@@ -5,7 +5,24 @@ namespace App\Models\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+
 class Question extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Replay::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
